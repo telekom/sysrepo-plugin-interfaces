7365d719fd7b8505511c2e1b3f8057684d7754ab
Mateo Cindric <mateo.cindric@sartura.hr>
2021-02-22 16:51:43 +0100

    minor update

--- src/interfaces.c
+++ src/interfaces.c
@@ -1,8 +1,22 @@
 #include <sysrepo.h>
+#include "utils/memory.h"
+#include <string.h>
 
-static int system_module_change_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, sr_event_t event, uint32_t request_id, void *private_data);
-static int system_state_data_cb(sr_session_ctx_t *session, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data);
-static int system_rpc_cb(sr_session_ctx_t *session, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data);
+#define BASE_YANG_MODEL "ietf-interfaces"
+
+// config data
+#define INTERFACES_YANG_MODEL "/" BASE_YANG_MODEL ":interfaces"
+#define INTERFACE_LIST_YANG_PATH INTERFACES_YANG_MODEL "/interface"
+
+// state data
+#define INTERFACES_STATE_YANG_MODEL "/" BASE_YANG_MODEL ":interfaces-state"
+
+// callbacks
+static int interfaces_module_change_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, sr_event_t event, uint32_t request_id, void *private_data);
+static int interfaces_state_data_cb(sr_session_ctx_t *session, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data);
+
+// helper functions
+static char *interfaces_xpath_get(const struct lyd_node *node);
 
 int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_data)
 {
@@ -23,6 +37,19 @@ int sr_plugin_init_cb(sr_session_ctx_t *session, void **private_data)
 	}
 
 	*private_data = startup_session;
+
+	// sub to any module change - for now
+	error = sr_module_change_subscribe(session, BASE_YANG_MODEL, "/" BASE_YANG_MODEL ":*//*", interfaces_module_change_cb, *private_data, 0, SR_SUBSCR_DEFAULT, &subscription);
+	if (error) {
+		SRP_LOG_ERR("sr_module_change_subscribe error (%d): %s", error, sr_strerror(error));
+		goto error_out;
+	}
+
+	error = sr_oper_get_items_subscribe(session, BASE_YANG_MODEL, INTERFACES_STATE_YANG_MODEL, interfaces_state_data_cb, NULL, SR_SUBSCR_CTX_REUSE, &subscription);
+	if (error) {
+		SRP_LOG_ERR("sr_oper_get_items_subscribe error (%d): %s", error, sr_strerror(error));
+		goto error_out;
+	}
 
 	SRP_LOG_INFMSG("plugin init done");
 
@@ -46,22 +73,110 @@ void sr_plugin_cleanup_cb(sr_session_ctx_t *session, void *private_data)
 	SRP_LOG_INFMSG("plugin cleanup finished");
 }
 
-static int system_module_change_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, sr_event_t event, uint32_t request_id, void *private_data)
+static int interfaces_module_change_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, sr_event_t event, uint32_t request_id, void *private_data)
 {
 	int error = 0;
+	sr_session_ctx_t *startup_session = (sr_session_ctx_t *) private_data;
+	sr_change_iter_t *system_change_iter = NULL;
+	sr_change_oper_t operation = SR_OP_CREATED;
+	const struct lyd_node *node = NULL;
+	const char *prev_value = NULL;
+	const char *prev_list = NULL;
+	bool prev_default = false;
+	char *node_xpath = NULL;
+	const char *node_value = NULL;
+	struct lyd_node_leaf_list *node_leaf_list;
+	struct lys_node_leaf *schema_node_leaf;
+
+	SRP_LOG_INF("module_name: %s, xpath: %s, event: %d, request_id: %u", module_name, xpath, event, request_id);
+
+	if (event == SR_EV_ABORT) {
+		SRP_LOG_ERR("aborting changes for: %s", xpath);
+		error = -1;
+		goto error_out;
+	}
+
+	if (event == SR_EV_DONE) {
+		error = sr_copy_config(startup_session, BASE_YANG_MODEL, SR_DS_RUNNING, 0, 0);
+		if (error) {
+			SRP_LOG_ERR("sr_copy_config error (%d): %s", error, sr_strerror(error));
+			goto error_out;
+		}
+	}
+
+	if (event == SR_EV_CHANGE) {
+		error = sr_get_changes_iter(session, xpath, &system_change_iter);
+		if (error) {
+			SRP_LOG_ERR("sr_get_changes_iter error (%d): %s", error, sr_strerror(error));
+			goto error_out;
+		}
+		while (sr_get_change_tree_next(session, system_change_iter, &operation, &node, &prev_value, &prev_list, &prev_default) == SR_ERR_OK) {
+			node_xpath = interfaces_xpath_get(node);
+
+			if (node->schema->nodetype == LYS_LEAF || node->schema->nodetype == LYS_LEAFLIST) {
+				node_leaf_list = (struct lyd_node_leaf_list *) node;
+				node_value = node_leaf_list->value_str;
+				if (node_value == NULL) {
+					schema_node_leaf = (struct lys_node_leaf *) node_leaf_list->schema;
+					node_value = schema_node_leaf->dflt ? schema_node_leaf->dflt : "";
+				}
+			}
+
+			SRP_LOG_DBG("node_xpath: %s; prev_val: %s; node_val: %s; operation: %d", node_xpath, prev_value, node_value, operation);
+
+			if (node->schema->nodetype == LYS_LEAF || node->schema->nodetype == LYS_LEAFLIST) {
+				if (operation == SR_OP_CREATED || operation == SR_OP_MODIFIED) {
+					if (error) {
+						SRP_LOG_ERR("set_config_value error (%d)", error);
+						goto error_out;
+					}
+				}
+			}
+			FREE_SAFE(node_xpath);
+			node_value = NULL;
+		}
+	}
+	goto out;
+
+error_out:
+	// nothing for now
+out:
+	FREE_SAFE(node_xpath);
+	sr_free_change_iter(system_change_iter);
 	return error ? SR_ERR_CALLBACK_FAILED : SR_ERR_OK;
 }
 
-static int system_state_data_cb(sr_session_ctx_t *session, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data)
+static int interfaces_state_data_cb(sr_session_ctx_t *session, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data)
 {
 	int error = SR_ERR_OK;
 	return error ? SR_ERR_CALLBACK_FAILED : SR_ERR_OK;
 }
 
-static int system_rpc_cb(sr_session_ctx_t *session, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
+static char *interfaces_xpath_get(const struct lyd_node *node)
 {
-	int error = SR_ERR_OK;
-	return error;
+	char *xpath_node = NULL;
+	char *xpath_leaflist_open_bracket = NULL;
+	size_t xpath_trimed_size = 0;
+	char *xpath_trimed = NULL;
+
+	if (node->schema->nodetype == LYS_LEAFLIST) {
+		xpath_node = lyd_path(node);
+		xpath_leaflist_open_bracket = strrchr(xpath_node, '[');
+		if (xpath_leaflist_open_bracket == NULL) {
+			return xpath_node;
+		}
+
+		xpath_trimed_size = (size_t) xpath_leaflist_open_bracket - (size_t) xpath_node + 1;
+		xpath_trimed = xcalloc(1, xpath_trimed_size);
+		strncpy(xpath_trimed, xpath_node, xpath_trimed_size - 1);
+		xpath_trimed[xpath_trimed_size - 1] = '\0';
+
+		FREE_SAFE(xpath_node);
+
+		return xpath_trimed;
+	} else {
+		return lyd_path(node);
+	}
 }
 
 #ifndef PLUGIN
