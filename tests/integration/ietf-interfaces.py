11f43fd64ec8843f5a93934f22a5b11989bca6fc
Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
2021-07-08 13:07:33 +0000

    integration tests: add license header to ietf-interfaces.py
    Signed-off-by: Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>

--- tests/integration/ietf-interfaces.py
+++ tests/integration/ietf-interfaces.py
@@ -1,3 +1,16 @@
+#
+# telekom / sysrepo-plugin-system
+#
+# This program is made available under the terms of the
+# BSD 3-Clause license which is available at
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# SPDX-FileCopyrightText: 2021 Deutsche Telekom AG
+# SPDX-FileContributor: Sartura Ltd.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
 import unittest
 import sysrepo
 import os
