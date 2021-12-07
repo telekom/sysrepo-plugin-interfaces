7be0a6b4eb6ad914d23ce77f18b01b33e22d481e
dbarac <dario.barac1@gmail.com>
2021-12-07 23:04:20 +0100

    add interface type change, rename, mtu and ipv6 forwarding integration tests

--- tests/integration/ietf-interfaces.py
+++ tests/integration/ietf-interfaces.py
@@ -241,6 +241,21 @@ class InterfaceTestCase(InterfacesTestCase):
         data.free()
         self.session.replace_config_ly(self.initial_data, 'ietf-interfaces')
 
+    def test_interface_lo_rename(self):
+        """Make sure that attempts to rename the loopback interface fail."""
+
+        with self.assertRaises(sysrepo.errors.SysrepoCallbackFailedError, msg="loopback renaming did not fail"):
+            self.edit_config("data/loopback_rename.xml")
+
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_lo_change_type(self):
+        """Make sure that attempts to change the loopback interface type fail."""
+
+        with self.assertRaises(sysrepo.errors.SysrepoCallbackFailedError, msg="loopback type change did not fail"):
+            self.edit_config("data/loopback_change_type.xml")
+
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
 
 class IpTestCase(InterfacesTestCase):
     def test_ip_addr_prefix_ipv4(self):
@@ -403,6 +418,68 @@ class IpTestCase(InterfacesTestCase):
         chosen_data.free()
         data.free()
         self.session.replace_config_ly(self.initial_data, 'ietf-interfaces')
+
+    def test_interface_mtu(self):
+        """Attempt to change loopback interface mtu."""
+        self.edit_config("data/loopback_mtu.xml")
+
+        expected_mtu = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<mtu>12345</mtu></ipv4></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv4/mtu')
+        mtu = data.print_mem("xml")
+        self.assertEqual(mtu, expected_mtu, "loopback MTU data is wrong")
+        with open('/sys/class/net/lo/mtu') as f:
+            mtu_value = f.read().strip()
+            self.assertEqual(mtu_value, '12345', 'plugin and system ipv4 MTU values differ.')
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_ipv6_mtu(self):
+        """Attempt to change loopback interface ipv6 mtu."""
+        self.edit_config("data/loopback_ipv6_mtu.xml")
+
+        expected_mtu = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<mtu>54321</mtu></ipv6></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv6/mtu')
+        mtu = data.print_mem("xml")
+        self.assertEqual(mtu, expected_mtu, "loopback ipv6 MTU data is wrong")
+
+        with open('/proc/sys/net/ipv6/conf/lo/mtu') as f:
+            mtu_value = f.read().strip()
+            self.assertEqual(mtu_value, '54321', 'plugin and system ipv6 MTU values differ.')
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_ipv6_forwarding(self):
+        """Attempt to enable loopback ipv6 address forwarding."""
+        self.edit_config("data/loopback_forwarding.xml")
+
+        expected_forwarding = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<forwarding>true</forwarding></ipv6></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv6/forwarding')
+        forwarding = data.print_mem("xml")
+        self.assertEqual(forwarding, expected_forwarding, "loopback forwarding data is wrong")
+
+        with open('/proc/sys/net/ipv6/conf/lo/forwarding') as f:
+            forwarding_configuration = f.read().strip()
+            self.assertEqual(
+                forwarding_configuration, '1',
+                'plugin and system interface state differ, forwarding is not enabled for loopback'
+            )
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
 
 
 if __name__ == '__main__':
