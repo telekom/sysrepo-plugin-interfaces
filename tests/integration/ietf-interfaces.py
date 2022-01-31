aedde87d6648594a832761f0bb6091a25a97b8d1
dbarac <35273740+dbarac@users.noreply.github.com>
2022-01-31 16:07:02 +0100

    interfaces-plugin: add integration tests (#10)
    * add interface type change, rename, mtu and ipv6 forwarding integration tests
    
    * add subinterface integration test
    
    * interfaces-plugin: add ipv4 forwarding integration test
    
    * interfaces-plugin: subinterface test - use first available ethernet interface as parent interface
    
    * interfaces-plugin: change MTU value in ipv6 MTU test
    
    * interfaces-plugin: update integration tests (restore config if assertRaises fails)

--- tests/integration/ietf-interfaces.py
+++ tests/integration/ietf-interfaces.py
@@ -66,11 +66,11 @@ class InterfacesTestCase(unittest.TestCase):
 
         self.session.apply_changes()
 
-    def edit_config_direct(self, xml):
+    def edit_config_direct(self, xml, config=True, strict=True, **parser_flags):
         ctx = self.conn.get_ly_ctx()
 
         data = xml
-        data = ctx.parse_data_mem(data, "xml", config=True, strict=True)
+        data = ctx.parse_data_mem(data, "xml", config=config, strict=strict, **parser_flags)
         self.session.edit_batch_ly(data)
         data.free()
 
@@ -241,6 +241,24 @@ class InterfaceTestCase(InterfacesTestCase):
         data.free()
         self.session.replace_config_ly(self.initial_data, 'ietf-interfaces')
 
+    def test_interface_lo_rename(self):
+        """Make sure that attempts to rename the loopback interface fail."""
+
+        try:
+            with self.assertRaises(sysrepo.errors.SysrepoCallbackFailedError, msg="loopback renaming did not fail"):
+                self.edit_config("data/loopback_rename.xml")
+        finally:
+            self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_lo_change_type(self):
+        """Make sure that attempts to change the loopback interface type fail."""
+
+        try:
+            with self.assertRaises(sysrepo.errors.SysrepoCallbackFailedError, msg="loopback type change did not fail"):
+                self.edit_config("data/loopback_change_type.xml")
+        finally:
+            self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
 
 class IpTestCase(InterfacesTestCase):
     def test_ip_addr_prefix_ipv4(self):
@@ -403,6 +421,160 @@ class IpTestCase(InterfacesTestCase):
         chosen_data.free()
         data.free()
         self.session.replace_config_ly(self.initial_data, 'ietf-interfaces')
+
+    def test_interface_mtu(self):
+        """Attempt to change loopback interface mtu."""
+        self.edit_config("data/loopback_mtu.xml")
+
+        expected_mtu = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<mtu>12345</mtu></ipv4></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv4/mtu')
+        mtu = data.print_mem("xml")
+        self.assertEqual(mtu, expected_mtu, "loopback MTU data is wrong")
+        with open('/sys/class/net/lo/mtu') as f:
+            mtu_value = f.read().strip()
+            self.assertEqual(mtu_value, '12345', 'plugin and system ipv4 MTU values differ.')
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_ipv6_mtu(self):
+        """Attempt to change loopback interface ipv6 mtu."""
+        self.edit_config("data/loopback_ipv6_mtu.xml")
+
+        expected_mtu = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<mtu>1300</mtu></ipv6></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv6/mtu')
+        mtu = data.print_mem("xml")
+        self.assertEqual(mtu, expected_mtu, "loopback ipv6 MTU data is wrong")
+
+        with open('/proc/sys/net/ipv6/conf/lo/mtu') as f:
+            mtu_value = f.read().strip()
+            self.assertEqual(mtu_value, '1300', 'plugin and system ipv6 MTU values differ.')
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_ipv4_forwarding(self):
+        """Attempt to enable loopback ipv4 address forwarding."""
+        self.edit_config("data/loopback_ipv4_forwarding.xml")
+
+        expected_forwarding = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<forwarding>true</forwarding></ipv4></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv4/forwarding')
+        forwarding = data.print_mem("xml")
+        self.assertEqual(forwarding, expected_forwarding, "loopback ipv4 forwarding data is wrong")
+
+        with open('/proc/sys/net/ipv4/conf/lo/forwarding') as f:
+            forwarding_configuration = f.read().strip()
+            self.assertEqual(
+                forwarding_configuration, '1',
+                'plugin and system interface state differ, forwarding is not enabled for loopback'
+            )
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_interface_ipv6_forwarding(self):
+        """Attempt to enable loopback ipv6 address forwarding."""
+        self.edit_config("data/loopback_ipv6_forwarding.xml")
+
+        expected_forwarding = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">' \
+        '<interface><name>lo</name><ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">' \
+        '<forwarding>true</forwarding></ipv6></interface></interfaces>'
+
+        data = self.session.get_data_ly('/ietf-interfaces:interfaces/interface[name="lo"]/ietf-ip:ipv6/forwarding')
+        forwarding = data.print_mem("xml")
+        self.assertEqual(forwarding, expected_forwarding, "loopback ipv6 forwarding data is wrong")
+
+        with open('/proc/sys/net/ipv6/conf/lo/forwarding') as f:
+            forwarding_configuration = f.read().strip()
+            self.assertEqual(
+                forwarding_configuration, '1',
+                'plugin and system interface state differ, forwarding is not enabled for loopback'
+            )
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+    def test_subinterface(self):
+        """
+        Attempt to create a subinterface.
+
+        The `sub-interfaces` feature of the `ietf-if-extensions` yang module should be
+        enabled before running this test.
+        """
+
+        # select a parent interface
+        all_interfaces = set(os.listdir('/sys/class/net/'))
+        parent_interface = None
+        for interface in all_interfaces:
+            p = subprocess.run(['ip', 'link', 'show', interface], capture_output=True, encoding="ascii")
+            if "link/ether" in p.stdout:
+                parent_interface = interface
+                break
+        self.assertIsNotNone(parent_interface, "no ethernet interfaces are available")
+
+        with open("data/subinterface.xml", "r") as f:
+            data = f.read()
+            data = data.format(PARENT_INTERFACE=parent_interface)
+            self.edit_config_direct(data, config=False, edit=True)
+
+        expected_subinterface = \
+        '<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"><interface>' \
+        '<name>{PARENT_INTERFACE}.sub1</name>' \
+        '<type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:l2vlan</type>' \
+        '<encapsulation xmlns="urn:ietf:params:xml:ns:yang:ietf-if-extensions">' \
+        '<dot1q-vlan xmlns="urn:ietf:params:xml:ns:yang:ietf-if-vlan-encapsulation">' \
+        '<outer-tag><tag-type xmlns:dot1q-types="urn:ieee:std:802.1Q:yang:ieee802-dot1q-types">' \
+        'dot1q-types:s-vlan</tag-type><vlan-id>10</vlan-id></outer-tag><second-tag>' \
+        '<tag-type xmlns:dot1q-types="urn:ieee:std:802.1Q:yang:ieee802-dot1q-types">' \
+        'dot1q-types:c-vlan</tag-type><vlan-id>20</vlan-id></second-tag></dot1q-vlan></encapsulation>' \
+        '<ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip"><address><ip>2001:db8:10::1</ip>' \
+        '<prefix-length>48</prefix-length></address>' \
+        '<dup-addr-detect-transmits>0</dup-addr-detect-transmits></ipv6>' \
+        '<parent-interface xmlns="urn:ietf:params:xml:ns:yang:ietf-if-extensions">' \
+        '{PARENT_INTERFACE}</parent-interface>' \
+        '</interface></interfaces>'.format(PARENT_INTERFACE=parent_interface)
+
+        path = '/ietf-interfaces:interfaces/interface[name="{PARENT_INTERFACE}.sub1"]'
+        data = self.session.get_data_ly(path.format(PARENT_INTERFACE=parent_interface))
+        subinterface_data = data.print_mem("xml")
+
+        self.assertEqual(subinterface_data, expected_subinterface, "subinterface data is wrong")
+
+        subinterface_name = parent_interface + ".sub1"
+        all_interfaces = set(os.listdir('/sys/class/net/'))
+        self.assertTrue(subinterface_name in all_interfaces)
+
+        # check ipv6 address and prefix-length
+        p = subprocess.run(
+            ['ip', 'addr', 'show', 'dev', subinterface_name], capture_output=True, encoding="ascii"
+        )
+        self.assertTrue("inet6 2001:db8:10::1/48" in p.stdout)
+
+        # check vlan
+        p = subprocess.run(
+            ['ip', '-d', 'link', 'show', subinterface_name], capture_output=True, encoding="ascii"
+        )
+        self.assertTrue("vlan protocol 802.1ad id 10" in p.stdout)
+
+        data.free()
+        self.session.replace_config_ly(self.initial_data, "ietf-interfaces")
+
+        # make sure the subinterface is deleted after replacing the configuration with initial data
+        all_interfaces = set(os.listdir('/sys/class/net/'))
+        self.assertTrue(subinterface_name not in all_interfaces)
 
 
 if __name__ == '__main__':
