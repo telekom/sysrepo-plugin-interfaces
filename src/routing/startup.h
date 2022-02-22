7c4f019ef5657c744ae0bd3427fc1bc23f601efb
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-02-22 12:00:13 +0000

    routing refactor: move initial load code into startup.c

--- src/routing/startup.h
+++ src/routing/startup.h
@@ -1,4 +1,10 @@
 #ifndef ROUTING_PLUGIN_STARTUP_H
 #define ROUTING_PLUGIN_STARTUP_H
 
+#include <routing/context.h>
+
+int routing_load_data(struct routing_ctx *ctx, sr_session_ctx_t *session);
+int routing_load_ribs(struct routing_ctx *ctx, sr_session_ctx_t *session, struct lyd_node *routing_container_node);
+int routing_load_control_plane_protocols(struct routing_ctx *ctx, sr_session_ctx_t *session, struct lyd_node *routing_container_node);
+
 #endif // ROUTING_PLUGIN_STARTUP_H
