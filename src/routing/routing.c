66cbc9329badfebd55a4c201c9ec0989d97c27d8
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-02-22 11:19:31 +0000

    routing refactor: move RPC subscription callback into separate file

--- src/routing/routing.c
+++ src/routing/routing.c
@@ -36,13 +36,11 @@
 
 // subs
 #include "subscription/change.h"
+#include "subscription/rpc.h"
 
 // operational callbacks
 static int routing_oper_get_rib_routes_cb(sr_session_ctx_t *session, uint32_t subscription_id, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data);
 static int routing_oper_get_interfaces_cb(sr_session_ctx_t *session, uint32_t subscription_id, const char *module_name, const char *path, const char *request_xpath, uint32_t request_id, struct lyd_node **parent, void *private_data);
-
-// rpc callbacks
-static int routing_rpc_active_route_cb(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data);
 
 // initial loading into the datastore
 static int routing_load_data(struct routing_ctx *ctx, sr_session_ctx_t *session);
@@ -667,13 +665,6 @@ out:
 	nl_socket_free(socket);
 	nl_cache_free(cache);
 	nl_cache_free(link_cache);
-	return error;
-}
-
-static int routing_rpc_active_route_cb(sr_session_ctx_t *session, uint32_t subscription_id, const char *op_path, const sr_val_t *input, const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
-{
-	int error = SR_ERR_OK;
-	SRPLG_LOG_DBG(PLUGIN_NAME, "xpath for RPC: %s", op_path);
 	return error;
 }
 
