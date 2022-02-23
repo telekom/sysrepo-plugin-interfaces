d019a81c32fb302cb369a8bb1ce0a6f49c94a4db
dbarac <35273740+dbarac@users.noreply.github.com>
2022-02-23 13:46:48 +0100

    interfaces-plugin: ipv6 MTU range check, update functions for /sys and /proc reading/writing
    * interfaces-plugin: update functions for /sys and /proc reading/writing
    
    * use strtol instead of atoi
    * add error checking
    * rename read_from_sys_file to read_interface_type_from_sys_file
    
    * interfaces-plugin: return error if new ipv6 MTU value is greater than the current ipv4 MTU
    
    * interfaces-plugin: add call rtnl_link_change after add_interface_ipv4
    
    The valid range for ipv6 MTU values depends on the current ipv4 MTU. If the ipv4 and ipv6
    MTU are both changed in the same update, rtnl_link_change has to be called before add_interface_ipv6,
    otherwise the old ipv4 MTU value would be used for range checking and the update could fail.

--- src/interfaces/interfaces.c
+++ src/interfaces/interfaces.c
@@ -90,7 +90,7 @@ static int remove_ipv6_address(ip_address_list_t *addr_list, struct nl_sock *soc
 static int remove_neighbors(ip_neighbor_list_t *nbor_list, struct nl_sock *socket, int addr_ver, int if_index);
 int write_to_proc_file(const char *dir_path, char *interface, const char *fn, int val);
 static int read_from_proc_file(const char *dir_path, char *interface, const char *fn, int *val);
-static int read_from_sys_file(const char *dir_path, char *interface, int *val);
+static int read_interface_type_from_sys_file(const char *dir_path, char *interface, int *val);
 int delete_config_value(const char *xpath, const char *value);
 int update_link_info(link_data_list_t *ld, sr_change_oper_t operation);
 static char *convert_ianaiftype(char *iana_if_type);
@@ -1315,13 +1315,20 @@ int update_link_info(link_data_list_t *ld, sr_change_oper_t operation)
 				goto out;
 			}
 
+			// the interface with name already exists, change it
+			error = rtnl_link_change(socket, old, request, 0);
+			if (error != 0) {
+				SRPLG_LOG_ERR(PLUGIN_NAME, "rtnl_link_change error (%d): %s", error, nl_geterror(error));
+				goto out;
+			}
+
 			error = add_interface_ipv6(&ld->links[i], old, request, rtnl_link_get_ifindex(old));
 			if (error != 0) {
 				SRPLG_LOG_ERR(PLUGIN_NAME, "add_interface_ipv6 error");
 				goto out;
 			}
 
-			// the interface with name already exists, change it
+			// update ipv6 config
 			error = rtnl_link_change(socket, old, request, 0);
 			if (error != 0) {
 				SRPLG_LOG_ERR(PLUGIN_NAME, "rtnl_link_change error (%d): %s", error, nl_geterror(error));
@@ -1384,13 +1391,19 @@ int update_link_info(link_data_list_t *ld, sr_change_oper_t operation)
 					SRPLG_LOG_ERR(PLUGIN_NAME, "add_interface_ipv4 error");
 					goto out;
 				}
-
+				// ipv4 config
+				error = rtnl_link_change(socket, old, request, 0);
+				if (error != 0) {
+					SRPLG_LOG_ERR(PLUGIN_NAME, "rtnl_link_change error (%d): %s", error, nl_geterror(error));
+					goto out;
+				}
 				error = add_interface_ipv6(&ld->links[i], old, request, rtnl_link_get_ifindex(old));
 				if (error != 0) {
 					SRPLG_LOG_ERR(PLUGIN_NAME, "add_interface_ipv6 error");
 					goto out;
 				}
 
+				// ipv6 config
 				error = rtnl_link_change(socket, old, request, 0);
 				if (error != 0) {
 					SRPLG_LOG_ERR(PLUGIN_NAME, "rtnl_link_change error (%d): %s", error, nl_geterror(error));
@@ -1697,8 +1710,17 @@ int add_interface_ipv6(link_data_t *ld, struct rtnl_link *old, struct rtnl_link 
 
 	// set mtu
 	if (ipv6->ip_data.mtu != 0) {
+		// If the new ipv6 MTU value is greater than link (ipv4) MTU, the
+		// kernel will return EINVAL when attempting to write
+		// the value to the MTU proc file.
+		if (ipv6->ip_data.mtu > ld->ipv4.mtu) {
+			SRPLG_LOG_ERR(PLUGIN_NAME, "Attempted to set ipv6 MTU value (%hd) greater than the current ipv4 MTU value (%hd).", ipv6->ip_data.mtu, ld->ipv4.mtu);
+			error = -1;
+			goto out;
+		}
 		error = write_to_proc_file(ipv6_base, if_name, "mtu", ipv6->ip_data.mtu);
 		if (error != 0) {
+			SRPLG_LOG_ERR(PLUGIN_NAME, "write_to_proc_file error (mtu)");
 			goto out;
 		}
 	}
@@ -2003,16 +2025,26 @@ int write_to_proc_file(const char *dir_path, char *interface, const char *fn, in
 	error = 0;
 
 	fptr = fopen((const char *) tmp_buffer, "w");
-
-	if (fptr != NULL) {
-		fprintf(fptr, "%d", val);
-		fclose(fptr);
-	} else {
+	if (fptr == NULL) {
 		SRPLG_LOG_ERR(PLUGIN_NAME, "failed to open %s: %s", tmp_buffer, strerror(errno));
 		error = -1;
 		goto out;
 	}
 
+	error = fprintf(fptr, "%d", val);
+	if (error < 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fprintf error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	error = 0;
+	error = fclose(fptr);
+	if (error != 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fclose error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
 out:
 	return error;
 }
@@ -2023,7 +2055,7 @@ static int read_from_proc_file(const char *dir_path, char *interface, const char
 	int error = 0;
 	char tmp_buffer[PATH_MAX];
 	FILE *fptr = NULL;
-	char tmp_val[2] = {0};
+	char val_str[20] = {0};
 
 	error = snprintf(tmp_buffer, sizeof(tmp_buffer), "%s/%s/%s", dir_path, interface, fn);
 	if (error < 0) {
@@ -2035,17 +2067,31 @@ static int read_from_proc_file(const char *dir_path, char *interface, const char
 	// snprintf returns return the number of bytes that are written
 	// reset error to 0
 	error = 0;
-
 	fptr = fopen((const char *) tmp_buffer, "r");
-
-	if (fptr != NULL) {
-		fgets(tmp_val, sizeof(tmp_val), fptr);
-
-		*val = atoi(tmp_val);
-
-		fclose(fptr);
-	} else {
+	if (fptr == NULL) {
 		SRPLG_LOG_ERR(PLUGIN_NAME, "failed to open %s: %s", tmp_buffer, strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	char *s = fgets(val_str, sizeof(val_str), fptr);
+	if (s == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fgets error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	errno = 0;
+	*val = (int) strtol(val_str, NULL, 10);
+	if (errno != 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "strtol error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	error = fclose(fptr);
+	if (error != 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fclose error: %s", strerror(errno));
 		error = -1;
 		goto out;
 	}
@@ -2054,12 +2100,12 @@ out:
 	return error;
 }
 
-static int read_from_sys_file(const char *dir_path, char *interface, int *val)
+static int read_interface_type_from_sys_file(const char *dir_path, char *interface, int *val)
 {
 	int error = 0;
 	char tmp_buffer[PATH_MAX];
 	FILE *fptr = NULL;
-	char tmp_val[4] = {0};
+	char val_str[20] = {0};
 
 	error = snprintf(tmp_buffer, sizeof(tmp_buffer), "%s/%s/type", dir_path, interface);
 	if (error < 0) {
@@ -2071,17 +2117,31 @@ static int read_from_sys_file(const char *dir_path, char *interface, int *val)
 	// snprintf returns return the number of bytes that are written
 	// reset error to 0
 	error = 0;
-
 	fptr = fopen((const char *) tmp_buffer, "r");
-
-	if (fptr != NULL) {
-		fgets(tmp_val, sizeof(tmp_val), fptr);
-
-		*val = atoi(tmp_val);
-
-		fclose(fptr);
-	} else {
+	if (fptr == NULL) {
 		SRPLG_LOG_ERR(PLUGIN_NAME, "failed to open %s: %s", tmp_buffer, strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	char *s = fgets(val_str, sizeof(val_str), fptr);
+	if (s == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fgets error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	errno = 0;
+	*val = (int) strtol(val_str, NULL, 10);
+	if (errno != 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "strtol error: %s", strerror(errno));
+		error = -1;
+		goto out;
+	}
+
+	error = fclose(fptr);
+	if (error != 0) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "fclose error: %s", strerror(errno));
 		error = -1;
 		goto out;
 	}
@@ -2156,9 +2216,9 @@ int add_existing_links(sr_session_ctx_t *session, link_data_list_t *ld)
 			const char *path_to_sys = "/sys/class/net/";
 			int type_id = 0;
 
-			error = read_from_sys_file(path_to_sys, name, &type_id);
+			error = read_interface_type_from_sys_file(path_to_sys, name, &type_id);
 			if (error != 0) {
-				SRPLG_LOG_ERR(PLUGIN_NAME, "read_from_sys_file error");
+				SRPLG_LOG_ERR(PLUGIN_NAME, "read_interface_type_from_sys_file error");
 				goto error_out;
 			}
 
