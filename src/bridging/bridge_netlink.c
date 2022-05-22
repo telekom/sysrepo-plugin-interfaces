602451e7409a97d25a2491dc97c848d4b7a25f92
dbarac <dario.barac1@gmail.com>
2022-05-22 12:57:48 +0200

    bridging plugin: add bridge_set_vlan_config function

--- src/bridging/bridge_netlink.c
+++ src/bridging/bridge_netlink.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <linux/if_ether.h>
+#include <linux/if_arp.h>
 #include <netlink/socket.h>
 #include <netlink/route/link.h>
 #include <sysrepo.h>
@@ -12,10 +13,11 @@ int bridge_get_vlan_info(struct nl_sock *socket, struct rtnl_link *bridge_link, 
 	struct nl_msg *msg = NULL;
 	unsigned char *msg_buf = NULL;
 	int error = 0;
+	int len = 0;
 
 	if (vlan_info == NULL) {
-		SRPLG_LOG_ERR(PLUGIN_NAME, "vlan_info is NULL", error);
-		return -1;
+		SRPLG_LOG_ERR(PLUGIN_NAME, "vlan_info is NULL");
+		goto out;
 	}
 
 	int if_index = rtnl_link_get_ifindex(bridge_link);
@@ -23,19 +25,20 @@ int bridge_get_vlan_info(struct nl_sock *socket, struct rtnl_link *bridge_link, 
 
 	// send RTM_GETLINK message for the bridge
 	error = rtnl_link_build_get_request(if_index, name, &msg);
-	if (error < 0) {
+	if (error) {
 		SRPLG_LOG_ERR(PLUGIN_NAME, "rtnl_link_build_get_request() failed (%d)", error);
 		goto out;
 	}
-	error = nl_send_auto(socket, msg);
-	if (error < 0) {
+	len = nl_send_auto(socket, msg);
+	if (len < 0) {
+		error = len;
 		SRPLG_LOG_ERR(PLUGIN_NAME, "nl_send_auto() failed (%d)", error);
 		goto out;
 	}
 
 	// wait for kernel response and ack
 	struct sockaddr_nl nla = {0};
-	int len = nl_recv(socket, &nla, &msg_buf, NULL);
+	len = nl_recv(socket, &nla, &msg_buf, NULL);
 	if (len <= 0) {
 		error = len;
 		SRPLG_LOG_ERR(PLUGIN_NAME, "nl_recv() failed (%d)", error);
@@ -100,5 +103,127 @@ out:
 		free(msg_buf);
 	}
 
+	return error;
+}
+
+int send_nl_request_and_error_check(struct nl_sock *socket, struct nl_msg *msg)
+{
+	unsigned char *msg_buf = NULL;
+	int error = 0;
+
+	int len = nl_send_auto(socket, msg);
+	if (len < 0) {
+		error = len;
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nl_send_auto() failed (%d)", error);
+		goto out;
+	}
+	struct sockaddr_nl nla = {0};
+	len = nl_recv(socket, &nla, &msg_buf, NULL);
+	if (len <= 0) {
+		error = len;
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nl_recv() failed (%d)", error);
+		goto out;
+	}
+	struct nlmsghdr *hdr = (struct nlmsghdr *) msg_buf;
+	if (hdr->nlmsg_type != NLMSG_ERROR) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "unexpected message type in received response (%d)", hdr->nlmsg_type);
+		goto out;
+	}
+	struct nlmsgerr *ack = nlmsg_data(hdr);
+	if (ack == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nlmsg_data() failed");
+		error = -1;
+		goto out;
+	}
+	if (ack->error) {
+		error = ack->error;
+		SRPLG_LOG_ERR(PLUGIN_NAME, "netlink request failed (%d)", ack->error);
+	}
+out:
+	if (msg_buf) {
+		free(msg_buf);
+	}
+	return error;
+}
+
+int bridge_set_vlan_config(struct nl_sock *socket, int bridge_link_idx, bridge_vlan_info_t *vlan_info)
+{
+	struct nl_msg *msg = NULL;
+	struct nlattr *link_info = NULL;
+	struct nlattr *info_data = NULL;
+	int error = 0;
+
+	if (vlan_info == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "vlan_info is NULL");
+		goto out;
+	}
+	msg = nlmsg_alloc_simple(RTM_NEWLINK, NLM_F_REQUEST | NLM_F_ACK);
+	if (msg == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nlmsg_alloc() failed");
+		goto out;
+	}
+	// fill RTM_NEWLINK message header
+	struct ifinfomsg ifinfo = {
+		.ifi_family = AF_UNSPEC,
+		.ifi_type   = ARPHRD_NETROM,
+		.ifi_index  = bridge_link_idx,
+		.ifi_flags  = 0,
+		.ifi_change = 0
+	};
+	error = nlmsg_append(msg, &ifinfo, sizeof(ifinfo), nlmsg_padlen(sizeof(ifinfo)));
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nl_append() failed (%d)", error);
+		goto out;
+	}
+	// open nested attributes IFLA_LINKINFO->IFLA_INFO_DATA
+	link_info = nla_nest_start(msg, IFLA_LINKINFO);
+	if (link_info == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nlmsg_nest_start() failed");
+		error = -1;
+		goto out;
+	}
+	error = nla_put_string(msg, IFLA_INFO_KIND, "bridge");
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nla_put_string() failed");
+		goto out;
+	}
+	info_data = nla_nest_start(msg, IFLA_INFO_DATA);
+	if (info_data == NULL) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nlmsg_nest_start() failed");
+		error = -1;
+		goto out;
+	}
+	// add bridge vlan attributes
+	error = nla_put_u8(msg, IFLA_BR_VLAN_FILTERING, vlan_info->vlan_filtering);
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nla_put_u8() failed");
+		goto out;
+	}
+	if (vlan_info->vlan_proto) {
+		error = nla_put_u16(msg, IFLA_BR_VLAN_PROTOCOL, vlan_info->vlan_proto);
+		if (error) {
+			SRPLG_LOG_ERR(PLUGIN_NAME, "nla_put_u16() failed");
+			goto out;
+		}
+	}
+	// close nested attributes
+	error = nla_nest_end(msg, info_data);
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nla_nest_end() failed");
+		goto out;
+	}
+	error = nla_nest_end(msg, link_info);
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "nla_nest_end() failed");
+		goto out;
+	}
+	error = send_nl_request_and_error_check(socket, msg);
+	if (error) {
+		SRPLG_LOG_ERR(PLUGIN_NAME, "send_nl_request_and_error_check() failed");
+	}
+out:
+	if (msg != NULL) {
+		nlmsg_free(msg);
+	}
 	return error;
 }
