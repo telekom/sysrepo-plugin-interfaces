19bbb61c98a2446f8b43de4eab49bf9a59e50e90
dbarac <dario.barac1@gmail.com>
2022-06-18 21:20:34 +0200

    bridging-plugin: refactor bridge list change callback

--- src/bridging/bridge/vlan/list.h
+++ src/bridging/bridge/vlan/list.h
@@ -17,4 +17,6 @@ void bridge_vlan_list_free(bridge_vlan_list_element_t **head);
 
 bridge_vlan_list_element_t *bridge_vlan_list_from_vids_list(char *vids_list, uint16_t port_flags);
 
+char *vids_str_next_vlan(char *vids_str, struct bridge_vlan_info *vlan_info);
+
 #endif // BRIDGING_PLUGIN_BRIDGE_VLAN_LIST_H
