8e873884eae0ccd584136765126a2229c6ee3c1c
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-09-16 12:54:30 +0200

    interfaces-plugin: add ietf-ip ipv4 and ipv6 API

---
+++ src/interfaces/src/plugin/api/interfaces/interface/ipv4/address/change.h
@@ -0,0 +1,17 @@
+#ifndef INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV4_ADDRESS_CHANGE_H
+#define INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV4_ADDRESS_CHANGE_H
+
+#include <srpc.h>
+#include <utarray.h>
+
+int interfaces_interface_ipv4_address_change_netmask_init(void* priv);
+int interfaces_interface_ipv4_address_change_netmask(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx);
+void interfaces_interface_ipv4_address_change_netmask_free(void* priv);
+int interfaces_interface_ipv4_address_change_prefix_length_init(void* priv);
+int interfaces_interface_ipv4_address_change_prefix_length(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx);
+void interfaces_interface_ipv4_address_change_prefix_length_free(void* priv);
+int interfaces_interface_ipv4_address_change_ip_init(void* priv);
+int interfaces_interface_ipv4_address_change_ip(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx);
+void interfaces_interface_ipv4_address_change_ip_free(void* priv);
+
+#endif // INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV4_ADDRESS_CHANGE_H
---
+++ src/interfaces/src/plugin/api/interfaces/interface/ipv6/neighbor/change.h
@@ -0,0 +1,14 @@
+#ifndef INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV6_NEIGHBOR_CHANGE_H
+#define INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV6_NEIGHBOR_CHANGE_H
+
+#include <srpc.h>
+#include <utarray.h>
+
+int interfaces_interface_ipv6_neighbor_change_link_layer_address_init(void* priv);
+int interfaces_interface_ipv6_neighbor_change_link_layer_address(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx);
+void interfaces_interface_ipv6_neighbor_change_link_layer_address_free(void* priv);
+int interfaces_interface_ipv6_neighbor_change_ip_init(void* priv);
+int interfaces_interface_ipv6_neighbor_change_ip(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx);
+void interfaces_interface_ipv6_neighbor_change_ip_free(void* priv);
+
+#endif // INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV6_NEIGHBOR_CHANGE_H
