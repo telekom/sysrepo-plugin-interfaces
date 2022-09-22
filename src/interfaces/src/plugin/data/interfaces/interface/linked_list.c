473b324b77494a1a9b9184bbe7b6b87ea63f169a
agardijan <andrej.gardijan@sartura.hr>
2022-09-22 12:18:25 +0200

    interfaces-plugin: add linked list ipv4/6, neighbor API

---
+++ src/interfaces/src/plugin/data/interfaces/interface/linked_list.c
@@ -0,0 +1,174 @@
+#include "linked_list.h"
+#include "utils/memory.h"
+
+void *interfaces_interface_linked_list_ipv4_address_element_new(void)
+{
+    interfaces_interfaces_interface_ipv4_address_element_t *new_element = NULL;
+
+    new_element = xmalloc(sizeof(interfaces_interfaces_interface_ipv4_address_element_t));
+
+    /* NULL all address fields */
+    new_element->address = (interfaces_interfaces_interface_ipv4_address_t) {0};
+
+    return new_element;
+}
+
+void *interfaces_interface_linked_list_ipv4_neighbor_element_new(void)
+{
+    interfaces_interfaces_interface_ipv4_neighbor_element_t *new_element = NULL;
+
+    new_element = xmalloc(sizeof(interfaces_interfaces_interface_ipv4_neighbor_element_t));
+
+    /* NULL all address fields */
+    new_element->neighbor = (interfaces_interfaces_interface_ipv4_neighbor_t) {0};
+
+    return new_element;
+}
+
+void *interfaces_interface_linked_list_ipv6_address_element_new(void)
+{
+    interfaces_interfaces_interface_ipv6_address_element_t *new_element = NULL;
+
+    new_element = xmalloc(sizeof(interfaces_interfaces_interface_ipv6_address_element_t));
+
+    /* NULL all address fields */
+    new_element->address = (interfaces_interfaces_interface_ipv6_address_t) {0};
+
+    return new_element;
+}
+
+void *interfaces_interface_linked_list_ipv6_neighbor_element_new(void)
+{
+    interfaces_interfaces_interface_ipv6_neighbor_element_t *new_element = NULL;
+
+    new_element = xmalloc(sizeof(interfaces_interfaces_interface_ipv6_neighbor_element_t));
+
+    /* NULL all address fields */
+    new_element->neighbor = (interfaces_interfaces_interface_ipv6_neighbor_t) {0};
+
+    return new_element;
+}
+
+void interfaces_interface_linked_list_ipv4_address_free(interfaces_interfaces_interface_ipv4_address_element_t **ll)
+{
+    interfaces_interfaces_interface_ipv4_address_element_t *elem, *tmp;
+
+    INTERFACES_INTERFACE_LINKED_LIST_IPV4_FREE_ADDRESS(ll, elem, tmp);
+}
+
+void interfaces_interface_linked_list_ipv4_neighbor_free(interfaces_interfaces_interface_ipv4_neighbor_element_t **ll)
+{
+    interfaces_interfaces_interface_ipv4_neighbor_element_t *elem, *tmp;
+
+    INTERFACES_INTERFACE_LINKED_LIST_FREE_NEIGHBOR(ll, elem, tmp);
+}
+
+void interfaces_interface_linked_list_ipv6_address_free(interfaces_interfaces_interface_ipv6_address_element_t **ll)
+{
+    interfaces_interfaces_interface_ipv6_address_element_t *elem, *tmp;
+
+    INTERFACES_INTERFACE_LINKED_LIST_IPV6_FREE_ADDRESS(ll, elem, tmp);
+}
+
+void interfaces_interface_linked_list_ipv6_neighbor_free(interfaces_interfaces_interface_ipv6_neighbor_element_t **ll)
+{
+    interfaces_interfaces_interface_ipv6_neighbor_element_t *elem, *tmp; 
+    INTERFACES_INTERFACE_LINKED_LIST_FREE_NEIGHBOR(ll, elem, tmp);
+}
+
+int interfaces_interface_linked_list_ipv4_address_element_set_address(interfaces_interfaces_interface_ipv4_address_element_t **ll, interfaces_interfaces_interface_ipv4_address_t *address)
+{
+     
+    if ((*ll)->address.ip != NULL) {
+        FREE_SAFE((*ll)->address.ip);
+    }
+    if ((*ll)->address.subnet.prefix_length != NULL) {
+        FREE_SAFE((*ll)->address.subnet.prefix_length);
+    }
+
+    if (address != NULL) {
+        (*ll)->address.subnet.prefix_length = address->subnet.prefix_length;
+        /* deepcopy char * */
+        if (address->ip != NULL) {
+            (*ll)->address.ip = xstrdup(address->ip);
+        }
+        /* netmask is part of a union with prefix_length, doesn't have to be set */
+        if (address->subnet.netmask != NULL) {
+            (*ll)->address.subnet.netmask = xstrdup(address->subnet.netmask);
+            return (*ll)->address.ip == NULL || (*ll)->address.subnet.netmask == NULL;
+        }
+        return (*ll)->address.ip == NULL;
+    }
+
+    return 0;
+}
+
+int interfaces_interface_linked_list_ipv4_address_element_set_neighbor(interfaces_interfaces_interface_ipv4_neighbor_element_t **ll, interfaces_interfaces_interface_ipv4_neighbor_t *neighbor)
+{
+     
+    if ((*ll)->neighbor.ip != NULL) {
+        FREE_SAFE((*ll)->neighbor.ip);
+    }
+    if ((*ll)->neighbor.link_layer_address != NULL) {
+        FREE_SAFE((*ll)->neighbor.link_layer_address);
+    }
+
+    if (neighbor != NULL) {
+        if (neighbor->ip != NULL) {
+            (*ll)->neighbor.ip = xstrdup(neighbor->ip);
+        }
+        if (neighbor->link_layer_address != NULL) {
+            (*ll)->neighbor.link_layer_address = xstrdup(neighbor->link_layer_address);
+        }
+
+        return (*ll)->neighbor.ip == NULL || (*ll)->neighbor.link_layer_address == NULL;
+    }
+
+    return 0;
+}
+
+int interfaces_interface_linked_list_ipv6_address_element_set_address(interfaces_interfaces_interface_ipv6_address_element_t **ll, interfaces_interfaces_interface_ipv6_address_t *address)
+{
+     
+    if ((*ll)->address.ip != NULL) {
+        FREE_SAFE((*ll)->address.ip);
+    }
+    if ((*ll)->address.prefix_length != NULL) {
+        FREE_SAFE((*ll)->address.prefix_length);
+    }
+
+    if (address != NULL) {
+        (*ll)->address.prefix_length = address->prefix_length;
+        /* deepcopy char * */
+        if (address->ip != NULL) {
+            (*ll)->address.ip = xstrdup(address->ip);
+        }
+        return (*ll)->address.ip == NULL;
+    }
+
+    return 0;
+}
+
+int interfaces_interface_linked_list_ipv6_address_element_set_neighbor(interfaces_interfaces_interface_ipv6_neighbor_element_t **ll, interfaces_interfaces_interface_ipv6_neighbor_t *neighbor)
+{
+     
+    if ((*ll)->neighbor.ip != NULL) {
+        FREE_SAFE((*ll)->neighbor.ip);
+    }
+    if ((*ll)->neighbor.link_layer_address != NULL) {
+        FREE_SAFE((*ll)->neighbor.link_layer_address);
+    }
+
+    if (neighbor != NULL) {
+        if (neighbor->ip != NULL) {
+            (*ll)->neighbor.ip = xstrdup(neighbor->ip);
+        }
+        if (neighbor->link_layer_address != NULL) {
+            (*ll)->neighbor.link_layer_address = xstrdup(neighbor->link_layer_address);
+        }
+
+        return (*ll)->neighbor.ip == NULL || (*ll)->neighbor.link_layer_address == NULL;
+    }
+
+    return 0;
+}
---
+++ src/interfaces/src/plugin/data/interfaces/interface/linked_list.h
@@ -0,0 +1,122 @@
+#ifndef INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
+#define INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
+
+#include "plugin/types.h"
+
+#include <utlist.h>
+#include <string.h>
+
+/*
+    Linked list operations
+*/
+
+#define INTERFACES_INTERFACE_LINKED_LIST_NEW(ll_ptr)    \
+    do                                                  \
+    {                                                   \
+        (ll_ptr) = NULL;                                \
+    } while(0)                                          
+
+#define INTERFACES_INTERFACE_LINKED_LIST_IPV4_FREE_ADDRESS(ll_ptr, elem_ptr, tmp_ptr)   \
+    do                                                                                  \
+    {                                                                                   \
+        LL_FOREACH_SAFE(*ll_ptr, elem_ptr, tmp_ptr) {                                   \
+            LL_DELETE(*ll_ptr, elem_ptr);                                               \
+            if (elem_ptr) {                                                             \
+                if (elem_ptr->address.ip != NULL) {                                     \
+                    FREE_SAFE(elem_ptr->address.ip);                                    \
+                }                                                                       \
+                if (elem_ptr->address.subnet.netmask != NULL) {                         \
+                    FREE_SAFE(elem_ptr->address.subnet.netmask);                        \
+                }                                                                       \
+                FREE_SAFE(elem_ptr);                                                    \
+            }                                                                           \
+        }                                                                               \
+    }                                                                                   \
+    while (0)
+
+#define INTERFACES_INTERFACE_LINKED_LIST_IPV6_FREE_ADDRESS(ll_ptr, elem_ptr, tmp_ptr)   \
+    do                                                                                  \
+    {                                                                                   \
+        LL_FOREACH_SAFE(*ll_ptr, elem_ptr, tmp_ptr) {                                   \
+            LL_DELETE(*ll_ptr, elem_ptr);                                               \
+            if (elem_ptr) {                                                             \
+                if (elem_ptr->address.ip != NULL) {                                     \
+                    FREE_SAFE(elem_ptr->address.ip);                                    \
+                }                                                                       \
+                FREE_SAFE(elem_ptr);                                                    \
+            }                                                                           \
+        }                                                                               \
+    }                                                                                   \
+    while (0)
+
+#define INTERFACES_INTERFACE_LINKED_LIST_FREE_NEIGHBOR(ll_ptr, elem_ptr, tmp_ptr)   \
+    do                                                                              \
+    {                                                                               \
+        LL_FOREACH_SAFE(*ll_ptr, elem_ptr, tmp_ptr) {                               \
+            LL_DELETE(*ll_ptr, elem_ptr);                                           \
+            if (elem_ptr) {                                                         \
+                if (elem_ptr->neighbor.ip != NULL) {                                \
+                    FREE_SAFE(elem_ptr->neighbor.ip);                               \
+                }                                                                   \
+                if (elem_ptr->neighbor.link_layer_address != NULL) {                \
+                    FREE_SAFE(elem_ptr->neighbor.link_layer_address);               \
+                }                                                                   \
+                FREE_SAFE(elem_ptr);                                                \
+            }                                                                       \
+        }                                                                           \
+    }                                                                               \
+    while (0)
+
+#define INTERFACES_INTERFACE_LINKED_LIST_FREE(ll_ptr)                                                                                \
+        _Generic((ll_ptr),                                                                                                           \
+                 interfaces_interfaces_interface_ipv4_address_element_t**  : interfaces_interface_linked_list_ipv4_address_free      \
+                 interfaces_interfaces_interface_ipv4_neighbor_element_t** : interfaces_interface_linked_list_ipv4_neighbor_free     \
+                 interfaces_interfaces_interface_ipv6_address_element_t**  : interfaces_interface_linked_list_ipv6_address_free      \
+                 interfaces_interfaces_interface_ipv6_neighbor_element_t** : interfaces_interface_linked_list_ipv6_neighbor_free     \
+        )(ll_ptr)
+
+/* prepend since ordering doesn't matter - O(1) */
+#define INTERFACES_INTERFACE_LINKED_LIST_ADD_ELEMENT(ll_ptr, new_element_ptr)   \
+    do                                                                          \
+    {                                                                           \
+        LL_PREPEND(ll_ptr, new_element_ptr);                                    \
+    } while(0)                                                                  \
+
+#define INTERFACES_INTERFACE_LINKED_LIST_GET_ELEMENT_STRING(ll_ptr, element_ptr, member, value)   \
+    do                                                                                            \
+    {                                                                                             \
+        LL_FOREACH(ll_ptr, element_ptr) {                                                         \
+            if (strcmp(element_ptr->member, value) == 0) {                                        \
+                break;                                                                            \
+            }                                                                                     \
+        }                                                                                         \
+    } while(0)                                                                         
+
+#define INTERFACES_INTERFACE_LINKED_LIST_GET_ELEMENT_SCALAR(ll_ptr, element_ptr, member, value)   \
+    do                                                                                            \
+    {                                                                                             \
+        LL_SEARCH_SCALAR(ll_ptr, element_ptr, member, value);                                     \
+    } while(0)                                                                         
+
+void interfaces_interface_linked_list_ipv4_address_free(interfaces_interfaces_interface_ipv4_address_element_t **ll);
+
+void interfaces_interface_linked_list_ipv4_neighbor_free(interfaces_interfaces_interface_ipv4_neighbor_element_t **ll);
+
+void interfaces_interface_linked_list_ipv6_address_free(interfaces_interfaces_interface_ipv6_address_element_t **ll);
+
+void interfaces_interface_linked_list_ipv6_neighbor_free(interfaces_interfaces_interface_ipv6_neighbor_element_t **ll);
+
+/*
+    Element operations
+*/
+
+void *interfaces_interface_linked_list_ipv4_address_element_new(void);
+void *interfaces_interface_linked_list_ipv4_neighbor_element_new(void);
+void *interfaces_interface_linked_list_ipv6_address_element_new(void);
+void *interfaces_interface_linked_list_ipv6_neighbor_element_new(void);
+
+int interfaces_interface_linked_list_ipv4_address_element_set_address(interfaces_interfaces_interface_ipv4_address_element_t **ll, interfaces_interfaces_interface_ipv4_address_t *address);
+int interfaces_interface_linked_list_ipv4_address_element_set_neighbor(interfaces_interfaces_interface_ipv4_neighbor_element_t **ll, interfaces_interfaces_interface_ipv4_neighbor_t *neighbor);
+
+#endif // INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
+
