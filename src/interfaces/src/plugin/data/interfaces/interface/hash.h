b78f3a875ae5dfc7d806901193752837b1f270d5
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-09-23 16:55:22 +0200

    interfaces-plugin: update interface hash element API

--- src/interfaces/src/plugin/data/interfaces/interface/hash.h
+++ src/interfaces/src/plugin/data/interfaces/interface/hash.h
@@ -21,19 +21,35 @@ void interfaces_interface_hash_free(interfaces_interface_hash_element_t** hash);
 */
 
 interfaces_interface_hash_element_t* interfaces_interface_hash_element_new(void);
+void interfaces_interface_hash_element_free(interfaces_interface_hash_element_t** el);
 int interfaces_interface_hash_element_set_name(interfaces_interface_hash_element_t** el, const char* name);
 int interfaces_interface_hash_element_set_description(interfaces_interface_hash_element_t** el, const char* description);
 int interfaces_interface_hash_element_set_type(interfaces_interface_hash_element_t** el, const char* type);
 int interfaces_interface_hash_element_set_enabled(interfaces_interface_hash_element_t** el, uint8_t enabled);
-int interfaces_interface_hash_element_set_link_up_down_trap_enable(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_link_up_down_trap_enable_t link_up_down_trap_enable);
-int interfaces_interface_hash_element_set_carrier_delay(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_carrier_delay_t carrier_delay);
-int interfaces_interface_hash_element_set_dampening(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_dampening_t dampening);
-int interfaces_interface_hash_element_set_encapsulation(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_encapsulation_t encapsulation);
+int interfaces_interface_hash_element_set_link_up_down_trap_enable(interfaces_interface_hash_element_t** el, interfaces_interface_link_up_down_trap_enable_t link_up_down_trap_enable);
+int interfaces_interface_hash_element_set_carrier_delay(interfaces_interface_hash_element_t** el, interfaces_interface_carrier_delay_t carrier_delay);
+int interfaces_interface_hash_element_set_dampening(interfaces_interface_hash_element_t** el, interfaces_interface_dampening_t dampening);
+int interfaces_interface_hash_element_set_encapsulation(interfaces_interface_hash_element_t** el, interfaces_interface_encapsulation_t encapsulation);
 int interfaces_interface_hash_element_set_loopback(interfaces_interface_hash_element_t** el, const char* loopback);
 int interfaces_interface_hash_element_set_max_frame_size(interfaces_interface_hash_element_t** el, uint32_t max_frame_size);
 int interfaces_interface_hash_element_set_parent_interface(interfaces_interface_hash_element_t** el, const char* parent_interface);
-int interfaces_interface_hash_element_set_ipv4(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_ipv4_t ipv4);
-int interfaces_interface_hash_element_set_ipv6(interfaces_interface_hash_element_t** el, interfaces_interfaces_interface_ipv6_t ipv6);
-void interfaces_interface_hash_element_free(interfaces_interface_hash_element_t** el);
+
+/*
+    IPv4
+*/
+
+int interfaces_interface_hash_element_set_ipv4(interfaces_interface_hash_element_t** el, interfaces_interface_ipv4_t ipv4);
+int interfaces_interface_hash_element_set_ipv4_enabled(interfaces_interface_hash_element_t** el, uint8_t enabled);
+int interfaces_interface_hash_element_set_ipv4_forwarding(interfaces_interface_hash_element_t** el, uint8_t forwarding);
+int interfaces_interface_hash_element_set_ipv4_mtu(interfaces_interface_hash_element_t** el, uint16_t mtu);
+
+/*
+    IPv6
+*/
+
+int interfaces_interface_hash_element_set_ipv6(interfaces_interface_hash_element_t** el, interfaces_interface_ipv6_t ipv6);
+int interfaces_interface_hash_element_set_ipv6_enabled(interfaces_interface_hash_element_t** el, uint8_t enabled);
+int interfaces_interface_hash_element_set_ipv6_forwarding(interfaces_interface_hash_element_t** el, uint8_t forwarding);
+int interfaces_interface_hash_element_set_ipv6_mtu(interfaces_interface_hash_element_t** el, uint16_t mtu);
 
 #endif // INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_HASH_H
