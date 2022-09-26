c17c0e7e20b80ffbafd67154fca440ecc44b9d4f
Mateo Cindrić <mateo.cindric0611@gmail.com>
2022-09-26 16:15:22 +0200

    interfaces-plugin: fix generic list free macro

--- src/interfaces/src/plugin/data/interfaces/interface/linked_list.h
+++ src/interfaces/src/plugin/data/interfaces/interface/linked_list.h
@@ -1,53 +1,57 @@
 #ifndef INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
 #define INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
 
-#include "ipv4/linked_list.h"
-#include "ipv6/linked_list.h"
+#include "ipv4/address.h"
+#include "ipv4/neighbor.h"
+#include "ipv6/address.h"
+#include "ipv6/neighbor.h"
 #include "plugin/types.h"
 
-#include <utlist.h>
 #include <string.h>
+#include <utlist.h>
 
 /*
     Linked list operations
 */
 
-#define INTERFACES_INTERFACE_LIST_NEW(ll_ptr)    \
-    do                                                  \
-    {                                                   \
-        (ll_ptr) = NULL;                                \
-    } while(0)                                          
+#define INTERFACES_INTERFACE_LIST_NEW(ll_ptr) \
+    do {                                      \
+        (ll_ptr) = NULL;                      \
+    } while (0)
 
-#define INTERFACES_INTERFACE_LIST_FREE(ll_ptr)                                                                         \
-        _Generic((ll_ptr),                                                                                                    \
-                 interfaces_interfaces_interface_ipv4_address_element_t**  : interfaces_interface_ipv4_address_list_free      \
-                 interfaces_interfaces_interface_ipv4_neighbor_element_t** : interfaces_interface_ipv4_neighbor_list_free     \
-                 interfaces_interfaces_interface_ipv6_address_element_t**  : interfaces_interface_ipv6_address_list_free      \
-                 interfaces_interfaces_interface_ipv6_neighbor_element_t** : interfaces_interface_ipv6_neighbor_list_free     \
-        )(ll_ptr)
+#define INTERFACES_INTERFACE_LIST_FREE(ll_ptr)                               \
+    _Generic((ll_ptr),                                                       \
+             interfaces_interfaces_interface_ipv4_address_element_t *        \
+             : interfaces_interface_ipv4_address_free                        \
+                     interfaces_interfaces_interface_ipv4_neighbor_element_t \
+                 *                                                           \
+             : interfaces_interface_ipv4_neighbor_free                       \
+                     interfaces_interfaces_interface_ipv6_address_element_t  \
+                 *                                                           \
+             : interfaces_interface_ipv6_address_free                        \
+                     interfaces_interfaces_interface_ipv6_neighbor_element_t \
+                 *                                                           \
+             : interfaces_interface_ipv6_neighbor_free)(ll_ptr)
 
 /* prepend since ordering doesn't matter - O(1) */
-#define INTERFACES_INTERFACE_LIST_ADD_ELEMENT(ll_ptr, new_element_ptr)   \
-    do                                                                          \
-    {                                                                           \
-        LL_PREPEND(ll_ptr, new_element_ptr);                                    \
-    } while(0)                                                                  \
+#define INTERFACES_INTERFACE_LIST_ADD_ELEMENT(ll_ptr, new_element_ptr) \
+    do {                                                               \
+        LL_PREPEND(ll_ptr, new_element_ptr);                           \
+    } while (0)
 
-#define INTERFACES_INTERFACE_LIST_GET_ELEMENT_STRING(ll_ptr, element_ptr, member, value)   \
-    do                                                                                            \
-    {                                                                                             \
-        LL_FOREACH(ll_ptr, element_ptr) {                                                         \
-            if (strcmp(element_ptr->member, value) == 0) {                                        \
-                break;                                                                            \
-            }                                                                                     \
-        }                                                                                         \
-    } while(0)                                                                         
+#define INTERFACES_INTERFACE_LIST_GET_ELEMENT_STRING(ll_ptr, element_ptr, member, value) \
+    do {                                                                                 \
+        LL_FOREACH(ll_ptr, element_ptr)                                                  \
+        {                                                                                \
+            if (strcmp(element_ptr->member, value) == 0) {                               \
+                break;                                                                   \
+            }                                                                            \
+        }                                                                                \
+    } while (0)
 
-#define INTERFACES_INTERFACE_LIST_GET_ELEMENT_SCALAR(ll_ptr, element_ptr, member, value)   \
-    do                                                                                            \
-    {                                                                                             \
-        LL_SEARCH_SCALAR(ll_ptr, element_ptr, member, value);                                     \
-    } while(0)                                                                         
+#define INTERFACES_INTERFACE_LIST_GET_ELEMENT_SCALAR(ll_ptr, element_ptr, member, value) \
+    do {                                                                                 \
+        LL_SEARCH_SCALAR(ll_ptr, element_ptr, member, value);                            \
+    } while (0)
 
 #endif // INTERFACES_PLUGIN_DATA_INTERFACES_INTERFACE_LIST_H
-
