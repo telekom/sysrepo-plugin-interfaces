57888931516fa19530ee7907b91958f2f2e6842c
Mateo Cindrić <mateocindricdev@gmail.com>
2022-10-12 20:56:39 +0200

    interfaces-plugin: implement IPv6 neighbor link-layer-address change callback

--- src/interfaces/src/plugin/api/interfaces/interface/ipv6/neighbor/change.c
+++ src/interfaces/src/plugin/api/interfaces/interface/ipv6/neighbor/change.c
@@ -20,20 +20,81 @@ int interfaces_interface_ipv6_neighbor_change_link_layer_address_init(void* priv
 int interfaces_interface_ipv6_neighbor_change_link_layer_address(void* priv, sr_session_ctx_t* session, const srpc_change_ctx_t* change_ctx)
 {
     int error = 0;
+    void* error_ptr = NULL;
+
+    // strings and buffers
     const char* node_name = LYD_NAME(change_ctx->node);
     const char* node_value = lyd_get_value(change_ctx->node);
+    char path_buffer[PATH_MAX] = { 0 };
+    char interface_name_buffer[100] = { 0 };
+    char ip_buffer[100] = { 0 };
+
+    // app context
+    interfaces_ctx_t* ctx = priv;
+
+    // mod changes context
+    interfaces_mod_changes_ctx_t* mod_ctx = &ctx->mod_ctx;
+
+    // libnl
+    struct rtnl_neigh* request_neigh = NULL;
+    struct rtnl_link* current_link = NULL;
+    struct nl_addr* dst_addr = NULL;
+    struct nl_addr* ll_addr = NULL;
 
     SRPLG_LOG_INF(PLUGIN_NAME, "Node Name: %s; Previous Value: %s, Value: %s; Operation: %d", node_name, change_ctx->previous_value, node_value, change_ctx->operation);
 
+    // get node path
+    SRPC_SAFE_CALL_PTR(error_ptr, lyd_path(change_ctx->node, LYD_PATH_STD, path_buffer, sizeof(path_buffer)), error_out);
+
+    // get interface name
+    SRPC_SAFE_CALL_ERR(error, srpc_extract_xpath_key_value(path_buffer, "interface", "name", interface_name_buffer, sizeof(interface_name_buffer)), error_out);
+
+    // get IP
+    SRPC_SAFE_CALL_ERR(error, srpc_extract_xpath_key_value(path_buffer, "neighbor", "ip", ip_buffer, sizeof(ip_buffer)), error_out);
+
+    SRPLG_LOG_INF(PLUGIN_NAME, "Node Path: %s; Interface Name: %s; Neighbor IP: %s", path_buffer, interface_name_buffer, ip_buffer);
+
+    // get link
+    SRPC_SAFE_CALL_PTR(current_link, rtnl_link_get_by_name(mod_ctx->nl_ctx.link_cache, interface_name_buffer), error_out);
+
     switch (change_ctx->operation) {
     case SR_OP_CREATED:
+        // not used - used only in IP change callback
         break;
     case SR_OP_MODIFIED:
+        // change lladdr
+        request_neigh = rtnl_neigh_alloc();
+
+        // set interface
+        rtnl_neigh_set_ifindex(request_neigh, rtnl_link_get_ifindex(current_link));
+
+        // parse destination and LL address
+        SRPC_SAFE_CALL_ERR(error, nl_addr_parse(ip_buffer, AF_INET6, &dst_addr), error_out);
+        SRPC_SAFE_CALL_ERR(error, nl_addr_parse(node_value, AF_LLC, &ll_addr), error_out);
+
+        // set destination and LL address
+        SRPC_SAFE_CALL_ERR(error, rtnl_neigh_set_dst(request_neigh, dst_addr), error_out);
+        rtnl_neigh_set_lladdr(request_neigh, ll_addr);
+
+        // change neighbor
+        SRPC_SAFE_CALL_ERR(error, rtnl_neigh_add(mod_ctx->nl_ctx.socket, request_neigh, NLM_F_REPLACE), error_out);
+
         break;
     case SR_OP_DELETED:
+        // not used - used only in IP change callback
         break;
     case SR_OP_MOVED:
         break;
+    }
+
+    goto out;
+
+error_out:
+    error = -1;
+
+out:
+    if (request_neigh) {
+        rtnl_neigh_put(request_neigh);
     }
 
     return error;
