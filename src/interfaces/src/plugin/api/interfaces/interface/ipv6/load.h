290bde5d528445190c480bbedee35b27118b2244
Mateo Cindric <mateocindricdev@gmail.com>
2022-10-31 20:45:41 +0000

    interfaces-plugin: load IPv6 address and neighbor list

--- src/interfaces/src/plugin/api/interfaces/interface/ipv6/load.h
+++ src/interfaces/src/plugin/api/interfaces/interface/ipv6/load.h
@@ -9,5 +9,7 @@
 int interfaces_interface_ipv6_load_enabled(interfaces_ctx_t* ctx, interfaces_interface_ipv6_t* ipv6, struct rtnl_link* link);
 int interfaces_interface_ipv6_load_forwarding(interfaces_ctx_t* ctx, interfaces_interface_ipv6_t* ipv6, struct rtnl_link* link);
 int interfaces_interface_ipv6_load_mtu(interfaces_ctx_t* ctx, interfaces_interface_ipv6_t* ipv6, struct rtnl_link* link);
+int interfaces_interface_ipv6_load_address(interfaces_ctx_t* ctx, interfaces_interface_ipv6_t* ipv6, struct rtnl_link* link);
+int interfaces_interface_ipv6_load_neighbor(interfaces_ctx_t* ctx, interfaces_interface_ipv6_t* ipv6, struct rtnl_link* link);
 
 #endif // INTERFACES_PLUGIN_API_INTERFACES_INTERFACE_IPV6_LOAD_H
