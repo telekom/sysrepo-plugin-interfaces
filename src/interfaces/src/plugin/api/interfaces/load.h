a4c3334e40ee5bab70aa6df9646f61ff63297524
Mateo Cindric <mateocindricdev@gmail.com>
2022-10-31 17:19:05 +0000

    interfaces-plugin: remove unused header

--- src/interfaces/src/plugin/api/interfaces/load.h
+++ src/interfaces/src/plugin/api/interfaces/load.h
@@ -4,7 +4,6 @@
 #include "plugin/common.h"
 #include "plugin/context.h"
 #include "plugin/data/interfaces/interface.h"
-#include "plugin/data/interfaces/interface/linked_list.h"
 
 int interfaces_load_interface(interfaces_ctx_t* ctx, interfaces_interface_hash_element_t** if_hash);
 
