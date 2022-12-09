85310bd7a586097969edee42b72586a43b061c01
andrej <you@example.com>
2022-12-09 12:15:55 +0100

    Revert "interfaces-plugin: change state name set return for NULL name"
    This reverts commit d55c8cae6773e263311ab64fd3a13319fb4a470d.

--- src/interfaces/src/plugin/data/interfaces/interface_state.c
+++ src/interfaces/src/plugin/data/interfaces/interface_state.c
@@ -71,7 +71,7 @@ int interfaces_interface_state_hash_element_set_name(interfaces_interface_state_
         return (*el)->state.name == NULL;
     }
 
-    return -1;
+    return 0;
 }
 
 void interfaces_interface_state_hash_element_set_state(interfaces_interface_state_hash_element_t** el, const uint8_t state)
