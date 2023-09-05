b039df9d76b6cb9dd171c360c353ae47e6f10ee7
Antonio Prcela <antonio.prcela@sartura.hr>
2023-09-05 14:17:17 +0200

    bridging-plugin: adapt incldue guards

--- src/bridging/plugin/startup/load.h
+++ src/bridging/plugin/startup/load.h
@@ -1,9 +1,9 @@
-#ifndef BRIDGING_PLUGIN_STARTUP_H
-#define BRIDGING_PLUGIN_STARTUP_H
+#ifndef BRIDGING_PLUGIN_STARTUP_LOAD_H
+#define BRIDGING_PLUGIN_STARTUP_LOAD_H
 
 #include "plugin/context.h"
 #include <sysrepo_types.h>
 
 int bridging_startup_load_data(bridging_ctx_t *ctx, sr_session_ctx_t *session);
 
-#endif // BRIDGING_PLUGIN_STARTUP_H
+#endif // BRIDGING_PLUGIN_STARTUP_LOAD_H
